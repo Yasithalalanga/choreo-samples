b0be22fba29f37e0d76944197c523d89ae3003d2
Yasitha Gamage <yasithamaxs@gmail.com>
2024-04-02 10:14:26 +0530

    Add a connection to go task

--- hello-world-go-task/main.go
+++ hello-world-go-task/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	svcURL := os.Getenv("SVC_URL")
+	fmt.Println("SVC_URL: ", svcURL)
+
 	for i, arg := range os.Args[1:] {
 		fmt.Println("Arg", i, ": ", arg)
 	}
