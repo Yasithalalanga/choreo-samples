2e3fe366b2705eabc15270df5b66782a3ec03e96
Yasitha Gamage <yasithamaxs@gmail.com>
2024-04-08 16:53:57 +0530

    Configs for ballerina service connection

--- greeting-service-go/main.go
+++ greeting-service-go/main.go
@@ -93,5 +93,5 @@ func greet(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Your service URL is %s, consumer key is %s, consumer secret is %s and token URL is %s\n", name, serviceUrl, consumerKey, consumerSecret, tokenUrl)
 	fmt.Fprintf(w, "Hello, %s!\n", name)
-	fmt.Fprintf(w, "Response from the service: %s\n", response)
+	fmt.Fprintf(w, "Response from the service: %s\n", response.Body)
 }
