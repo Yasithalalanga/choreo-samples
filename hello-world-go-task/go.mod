764731335398b69127398d269ad15020f7542136
Yasitha Gamage <yasithamaxs@gmail.com>
2024-04-10 09:34:22 +0530

    Add a connection to go task with usage support

--- hello-world-go-task/go.mod
+++ hello-world-go-task/go.mod
@@ -1,3 +1,5 @@
 module ontime
 
 go 1.19
+
+require golang.org/x/oauth2 v0.19.0
--- hello-world-go-task/main.go
+++ hello-world-go-task/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
 	"os"
+
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
@@ -10,6 +15,42 @@ func main() {
 
 	svcURL := os.Getenv("SVC_URL")
 	fmt.Println("SVC_URL: ", svcURL)
+
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	fmt.Println("CONSUMER_KEY: ", consumerKey)
+
+	consumerSecret := os.Getenv("CONSUMER_SECRET")
+	fmt.Println("CONSUMER_SECRET: ", consumerSecret)
+
+	tokenURL := os.Getenv("TOKEN_URL")
+	fmt.Println("TOKEN_URL: ", tokenURL)
+
+	var clientCredsConfig = clientcredentials.Config{
+		ClientID:     consumerKey,
+		ClientSecret: consumerSecret,
+		TokenURL:     tokenURL,
+	}
+	client := clientCredsConfig.Client(context.Background())
+
+	//sample go code snippet
+	response, err := client.Get(svcURL + "?name=hello")
+	if err != nil {
+		log.Printf("Error occurred while calling the service: %v", err)
+		return
+	}
+
+	defer response.Body.Close() // Don't forget to close the body
+
+	// Read the content
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		// handle error
+		fmt.Println("Error reading the response body:", err)
+		return
+	}
+
+	// Convert the body to string and print
+	fmt.Println("Response from the service: %s\n", string(body))
 
 	for i, arg := range os.Args[1:] {
 		fmt.Println("Arg", i, ": ", arg)
