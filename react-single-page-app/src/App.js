7836667574875860eee44d3798ab55acd0494b1a
Yasitha Gamage <yasithamaxs@gmail.com>
2024-04-15 12:29:00 +0530

    Add Connection Support to WebApp (No Auth)

--- react-single-page-app/src/App.js
+++ react-single-page-app/src/App.js
@@ -1,7 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import oauth from 'axios-oauth-client'
+import axios from 'axios';
+
+async function App() {
+  const serviceURL = window?.configs?.serviceURL ? window.configs.serviceURL : "/";
+  const consumerKey = window?.configs?.consumerKey ? window.configs.consumerKey : "";
+  const consumerSecret = window?.configs?.consumerSecret ? window.configs.consumerSecret : "";
+  const tokenURL = window?.configs?.tokenURL ? window.configs.tokenURL : "";
+
+  console.log('serviceURL', serviceURL);
+  console.log('consumerKey', consumerKey);
+  console.log('consumerSecret', consumerSecret);
+  console.log('tokenURL', tokenURL);
+
+  const getClientCredentials = oauth.clientCredentials(
+    axios.create(),
+    tokenURL,
+   consumerKey,
+    consumerSecret
+  );
+  const auth = await getClientCredentials();
+  const accessToken = auth.access_token;
+
+  console.log('accessToken', accessToken);
+
+  let resourcePath = '?name=hello';
+  const response = await axios.get(serviceURL + resourcePath, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+
+  console.log('response', response);
+
+
   return (
     <div className="App">
       <header className="App-header">
