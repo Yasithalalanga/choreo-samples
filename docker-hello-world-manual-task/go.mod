1c49370d610e976b653b965ba8759658cfd50a8a
Yasitha Gamage <yasithamaxs@gmail.com>
2024-04-16 09:32:23 +0530

    Test BYOC Connection Support

--- docker-hello-world-manual-task/go.mod
+++ docker-hello-world-manual-task/go.mod
@@ -1,3 +1,5 @@
 module github.com/wso2/choreo-sample-apps/go/hello-world
 
 go 1.20
+
+require golang.org/x/oauth2 v0.19.0
--- docker-hello-world-manual-task/main.go
+++ docker-hello-world-manual-task/main.go
@@ -19,12 +19,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
 	"os"
+
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World! Docker Manual Task Check")
+
+	svcURL := os.Getenv("SVC_URL")
+	fmt.Println("SVC_URL: ", svcURL)
+
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	fmt.Println("CONSUMER_KEY: ", consumerKey)
+
+	consumerSecret := os.Getenv("CONSUMER_SECRET")
+	fmt.Println("CONSUMER_SECRET: ", consumerSecret)
+
+	tokenURL := os.Getenv("TOKEN_URL")
+	fmt.Println("TOKEN_URL: ", tokenURL)
+
+	var clientCredsConfig = clientcredentials.Config{
+		ClientID:     consumerKey,
+		ClientSecret: consumerSecret,
+		TokenURL:     tokenURL,
+	}
+	client := clientCredsConfig.Client(context.Background())
+
+	//sample go code snippet
+	response, err := client.Get(svcURL + "?name=hello")
+	if err != nil {
+		log.Printf("Error occurred while calling the service: %v", err)
+		return
+	}
+
+	defer response.Body.Close() // Don't forget to close the body
+
+	// Read the content
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		// handle error
+		fmt.Println("Error reading the response body:", err)
+		return
+	}
+
+	fmt.Println("This is connection call from manual task")
+
+	// Convert the body to string and print
+	fmt.Printf("Response from the service: %s\n", string(body))
+
 	for i, arg := range os.Args[1:] {
 		fmt.Println("Arg", i, ": ", arg)
 	}
