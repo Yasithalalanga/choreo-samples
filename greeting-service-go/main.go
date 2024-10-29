304b9103d75291b782e0891744e16205b41a648d
Yasitha Gamage <yasithamaxs@gmail.com>
2024-10-29 18:50:30 +0530

    Stripe & Redis Config Sample

--- greeting-service-go/main.go
+++ greeting-service-go/main.go
@@ -68,4 +68,78 @@ func greet(w http.ResponseWriter, r *http.Request) {
 		name = "Stranger"
 	}
 	fmt.Fprintf(w, "Hello, %s!\n", name)
+
+	// Read environment variables
+	currency := os.Getenv("CURRENCY")
+	apiURL := os.Getenv("STRIPE_API_URL")
+	secretKey := os.Getenv("STRIPE_SECRET_KEY")
+
+	fmt.Fprintln(w, "\n\nRead as Environment variables:")
+	fmt.Fprintln(w, "Currency:", currency)
+	fmt.Fprintln(w, "Stripe API URL:", apiURL)
+	fmt.Fprintln(w, "Stripe Secret Key:", secretKey)
+
+	// Read from files
+	fmt.Fprintln(w, "\n\nRead from files:")
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Fprintln(w, "Error reading config file: ", err)
+		return
+	}
+	defer configFile.Close()
+
+	buf := make([]byte, 1024)
+	n, err := configFile.Read(buf)
+	if err != nil {
+		fmt.Fprintln(w, "Error reading config file: ", err)
+		return
+	}
+
+	fmt.Fprintln(w, "Config file content: ", string(buf[:n]))
+
+	certFile, err := os.Open("cert.pem")
+	if err != nil {
+		fmt.Fprintln(w, "Error reading cert file: ", err)
+		return
+	}
+	defer certFile.Close()
+
+	buf = make([]byte, 1024)
+	n, err = certFile.Read(buf)
+	if err != nil {
+		fmt.Fprintln(w, "Error reading cert file: ", err)
+		return
+	}
+
+	fmt.Fprintln(w, "Cert file content: ", string(buf[:n]))
+
+	// Redis Sample
+	isRedisEnabled := os.Getenv("REDIS_ENABLED")
+	if isRedisEnabled == "true" {
+		// Connect to Redis
+		fmt.Fprintln(w, "\n\nRedis is enabled. Connecting to Redis...")
+		redisHost := os.Getenv("REDIS_HOST")
+		redisPort := os.Getenv("REDIS_PORT")
+		fmt.Fprintf(w, "Redis Host: %s\n", redisHost)
+		fmt.Fprintf(w, "Redis Port: %s\n", redisPort)
+
+		// Read from files
+		redisConfigFile, err := os.Open("etc/redis/redis.conf")
+		if err != nil {
+			fmt.Fprintln(w, "Error reading redis config file: ", err)
+			return
+		}
+
+		defer redisConfigFile.Close()
+
+		buf = make([]byte, 1024)
+		content, err := redisConfigFile.Read(buf)
+		if err != nil {
+			fmt.Fprintln(w, "Error reading redis config file: ", err)
+			return
+		}
+
+		fmt.Fprintln(w, "\nRedis Config file content:\n\n", string(buf[:content]))
+	}
+
 }
